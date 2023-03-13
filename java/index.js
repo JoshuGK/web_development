aac2f22cc8ab3fa2aca6da34efc42548c186d112
jog <gsakajoshua166@gmail.com>
2023-03-13 22:32:47 +0300

    modified

---
+++ java/index.js
@@ -0,0 +1,36 @@
+/*create variables*/
+let name
+var number= 48498590859045857857
+console.log(name)
+console.log(number)
+//operators
+let first_name="me "
+let sec_name="here"
+let full_name=first_name+sec_name
+console.log(full_name)
+//control structures
+var num=5
+if(num<10){
+    //code if condition is true console.log("number is less than 10")
+}else{console.log("number is greater than 20")
+    
+}
+
+//2
+x=15
+if (x>10){
+    console.log("greater than 20")
+}else if(x==10){
+    console.log("less than 10")
+}
+//nested if ie checks many conditions
+var country="togo"
+var age=17
+if(country=="togo"){
+    //code is true
+    if(age>=18){
+        console.log("accepted")
+    }else{
+    console.log("not accepted")
+    }
+}
